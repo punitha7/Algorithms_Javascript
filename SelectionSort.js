098337177c039b381d437be56753a85d94dfd41a
Punitha <punitha7@gmail.com>
2017-04-15 17:55:10 -0400

    Create SelectionSort.js

---
+++ SelectionSort.js
@@ -0,0 +1,41 @@
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+var indexOfMinimum = function(array, startIndex) {
+
+    var minValue = array[startIndex];
+    var minIndex = startIndex;
+
+    for(var i = minIndex + 1; i < array.length; i++) {
+        if(array[i] < minValue) {
+            minIndex = i;
+            minValue = array[i];
+        }
+    } 
+    return minIndex;
+}; 
+
+var selectionSort = function(array) {
+        var minIndex = 0;
+    var minValue = array[0];
+    for ( var i = 0; i < array.length; i++) {
+        swap(array, i, indexOfMinimum(array, i));
+    }
+
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+selectionSort(array);
+println("Array after sorting:  " + array);
+
+
+Program.assertEqual(array, [7, 9, 11, 22, 42, 88, 99]);
+array = [22, 11, 99, 88, 9, 7, 42,1];
+selectionSort(array);
+Program.assertEqual(array, [1, 7, 9, 11, 22, 42, 88, 99]);
+array = [22, 11, 0, 88, 9, 7, 42,1];
+selectionSort(array);
+Program.assertEqual(array, [0, 1, 7, 9, 11, 22, 42, 88]);
